a5ea4af2ca389248893f6f91929cb188e23d3c90
adilsqe13 <adilsqe13@gmail.com>
2024-01-06 22:04:00 +0530

    mongoose

--- functions/db.js
+++ functions/db.js
@@ -2,20 +2,26 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const mongoURI = process.env.MONGODB_URI;
 
-mongoose.connect(mongoURI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+exports.handler = async function (event, context) {
+  const client = await mongoose.connect(process.env.CONNECTIONSTRING, { useUnifiedTopology: true })
+  const db = client.db()
+
+
+// mongoose.connect(mongoURI, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   });
   
-  const db = mongoose.connection;
+  // const db = mongoose.connection;
   db.on('error', console.error.bind(console, 'MongoDB connection error:'));
   db.once('open', () => {
     console.log('Connected to the database');
   });
 
-  module.exports = async function connectToDatabase() {
-    await new Promise((resolve) => {
-      db.once('open', resolve);
-    });
-    return db;
-  };
+  // module.exports = async function connectToDatabase() {
+  //   await new Promise((resolve) => {
+  //     db.once('open', resolve);
+  //   });
+  //   return db;
+  // };
+}
