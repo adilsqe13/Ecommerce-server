2470a660fbcdc0ba23d379633f3e9b76c05712e0
adilsqe13 <adilsqe13@gmail.com>
2024-01-06 19:31:06 +0530

    o

--- functions/db.js
+++ functions/db.js
@@ -3,19 +3,38 @@ const mongoose = require('mongoose');
 const mongoURI = process.env.MONGODB_URI;
 
 mongoose.connect(mongoURI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-  
-  const db = mongoose.connection;
-  db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-  db.once('open', () => {
-    console.log('Connected to the database');
-  });
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-  exports.handler = async function connectToDatabase() {
-    await new Promise((resolve) => {
-      db.once('open', resolve);
-    });
-    return db;
-  };
+const db = mongoose.connection;
+
+db.on('error', (error) => {
+  console.error('MongoDB connection error:', error);
+});
+
+db.once('open', () => {
+  console.log('Connected to the database');
+});
+
+// Export a function that returns the database connection
+module.exports = async function connectToDatabase() {
+  return new Promise((resolve, reject) => {
+    // If the database connection is already established, resolve immediately
+    if (mongoose.connection.readyState === 1) {
+      console.log('Database connection already open');
+      resolve(db);
+    } else {
+      // If not, wait for the 'open' event or handle errors
+      db.once('open', () => {
+        console.log('Database connection established');
+        resolve(db);
+      });
+
+      db.on('error', (error) => {
+        console.error('MongoDB connection error:', error);
+        reject(error);
+      });
+    }
+  });
+};
