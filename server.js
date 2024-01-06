3ec35d19242106f9314f91193a26153710d70d5d
adilsqe13 <adilsqe13@gmail.com>
2024-01-06 12:19:15 +0530

    updates

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
   // Serve HTML file for the root path
