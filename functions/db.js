0800e641767d6f0ea012a195bc1912fa94ade257
adilsqe13 <adilsqe13@gmail.com>
2024-01-06 23:34:06 +0530

    gha

--- functions/db.js
+++ functions/db.js
@@ -8,9 +8,15 @@ mongoose.connect(mongoURI, {
   });
   
   const db = mongoose.connection;
-  db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-  db.once('open', () => {
-    console.log('Connected to the database');
-  });
+  // db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+  // db.once('open', () => {
+  //   console.log('Connected to the database');
+  // });
 
-  module.exports = connectToDatabase() ;
+  module.exports = db;
+  // module.exports = async function connectToDatabase() {
+  //   await new Promise((resolve) => {
+  //     db.once('open', resolve);
+  //   });
+  //   return db;
+  // };
