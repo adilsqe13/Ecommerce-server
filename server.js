16e713da6c88df1c9eeec53fa6d4ae7109041fd9
adilsqe13 <adilsqe13@gmail.com>
2024-01-06 12:12:32 +0530

    1st commit

---
+++ server.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.get('/', (req, res) => {
+  // Serve HTML file for the root path
+  const filePath = path.join(__dirname, 'index.html');
+  res.sendFile(filePath);
+});
+
+app.get('/api', (req, res) => {
+  // Respond to API GET request
+  const responseData = '<h1>Hello from API!</h1>';
+  res.send(responseData);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
