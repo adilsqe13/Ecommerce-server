ba5d1dd8346fce6beed272d91c1ed81bf1679c18
adilsqe13 <adilsqe13@gmail.com>
2024-01-07 17:13:07 +0530

    multer

--- functions/routes/seller/sellerProducts.js
+++ functions/routes/seller/sellerProducts.js
@@ -7,7 +7,7 @@ const fetchseller = require('../../middleware/fetchseller');
 const multer = require('multer');
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "../frontend/src/images/")
+    cb(null, "uploads")
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now();
